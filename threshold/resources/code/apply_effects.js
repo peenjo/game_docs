5b5c91dbad7a45026c78f0a2b8b156dbffa0f8db
Eric <peenjo@users.noreply.github.com>
2026-08-28 15:36:02 -0700

    simplified selecting effect targets for GM

--- threshold/resources/code/apply_effects.js
+++ threshold/resources/code/apply_effects.js
@@ -1,35 +1,21 @@
 //*********************************************
 // Given an array of effect names, this creates
 // and applies Foundry Active Effects to the
-// target. It also posts informational messages
+// target. It then posts informational messages
 // to the Foundry Chat Window.
 //*********************************************
 
-// get all currently targeted tokens by the user
-const targets = Array.from(game.user.targets);
-
-// ensure exactly one target is selected
-if (targets.length === 0) {
-    ui.notifications.warn("Please target a token first! (Use the Target tool or hover and press 'T')");
+const selectedTokens = canvas.tokens.controlled;
+// ensure exactly one token is selected
+if (selectedTokens.length === 0) {
+    ui.notifications.warn("Please select a token first!");
     return null;
 }
-if (targets.length > 1) {
-    ui.notifications.warn("Too many targets! Please target only ONE token at a time to stun.");
+if (selectedTokens.length > 1) {
+    ui.notifications.warn("Too many selected tokens! Please select only ONE token at a time.");
     return null;
 }
-
-// grab the single targeted token
-const target = targets[0];
-
-// get the user's controlled token/actor doing the action
-// TODO ech 2026-08-28 - not sure this is the way to go
-const attacker = canvas.tokens.controlled[0];
-
-// ensure the user actually has a token selected to perform the action
-if (!attacker) { // TODO ech 2026-08-27 - not a totally accurate message for a GM
-    ui.notifications.warn("Please select your own character token first to perform the action!");
-    return null;
-}
+const target = selectedTokens[0];
 
 // list of effects passed in from caller macro
 const activeEffects = scope.activeEffects;
@@ -48,7 +34,7 @@ try {
     await target.actor.createEmbeddedDocuments("ActiveEffect", effects);
 } catch (error) {
     // TODO ech 2026-08-28 - this masks the actual error depending on how it's handled in the module code
-    ui.notifications.warn("Only a Gamemaster can perform this action.");
+    ui.notifications.warn("Only a GameMaster can perform this action.");
     return null;
 }
 
@@ -62,8 +48,5 @@ for (const effect of effects) {
   `;
 
     // post the chat message object in Foundry
-    await ChatMessage.create({
-        content: chatContent, speaker: ChatMessage.getSpeaker({token: attacker})
-    });
-
+    await ChatMessage.create({content: chatContent});
 }
