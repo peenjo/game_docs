b6d4ed543c166ad0dce3541afc18dc162a8cb39c
Eric <peenjo@users.noreply.github.com>
2026-08-28 15:40:47 -0700

    renamed file

--- threshold/resources/code/example roll table macro.js
+++ threshold/resources/code/example roll table macro.js
@@ -1,3 +1,10 @@
+//***********************************
+// An example of the over 200 macros
+// created, one for each entry in the
+// six special effects rollable tables.
+// Only line 12 differs in each case.
+//***********************************
+
 const getGlobalEffectNames = game.macros.getName("Global_Effect_Names");
 const EFFECTS = await getGlobalEffectNames.execute();
 
@@ -5,4 +12,4 @@ const EFFECTS = await getGlobalEffectNames.execute();
 const effects = [EFFECTS.MOVED_SHORT_BACK, EFFECTS.STUNNED];
 
 const applyEffects = game.macros.getName("Apply_Effects");
-await applyEffects.execute({activeEffects : effects});
+await applyEffects.execute({activeEffects: effects});
