01dc85ca17b3c72e765437efc25c6ef1b50ffacb
Eric <peenjo@users.noreply.github.com>
2026-09-08 16:53:19 -0700

    delete zombie expired events from mark_as_moved

--- threshold/resources/code/mark_as_moved.js
+++ threshold/resources/code/mark_as_moved.js
@@ -6,6 +6,15 @@
 // track NPC movement in a combat turn.
 //*********************************************
 
+// ech 2026-09-08 - This makes all the expired events
+// actually go away instead of staying in the event tab
+// as zombie entries. It only needs to be set once on
+// game load, but that involves
+// Hooks.once("init", () => {...
+// code in a custom module which turns into a whole
+// thing. This is redundant but WAY simpler.
+CONFIG.ActiveEffect.expiryAction = 'delete';
+
 const selectedTokens = canvas.tokens.controlled;
 // ensure exactly one token is selected
 if (selectedTokens.length === 0) {
@@ -21,8 +30,8 @@ const target = selectedTokens[0];
 const effectData = {
     name: "Character Moved",
     img: "icons/svg/walk.svg",
-    duration: {turns: 0, expiry: "roundEnd"}, // clear at end of every round
+    duration: {turns: 0, expiry: "roundEnd"},
 };
 
-// apply the active effect to the selected token's actor
+// apply the all active effects directly to the targeted token's actor
 await target.actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
